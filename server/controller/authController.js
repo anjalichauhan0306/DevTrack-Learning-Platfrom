7c08c873883ec0f5d81d332355a932d1342801a1
Anjali Chauhan <ac1447703@gmail.com>
2026-01-19 23:04:12 +0530

    Update .gitignore, fix import path in index.js, add user model, and create authController

---
+++ server/controller/authController.js
@@ -0,0 +1,11 @@
+
+
+export const signUp = async (req , res) => {
+    try {
+        
+        
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 import express from "express"
 import dotenv from 'dotenv'
-import { db } from "./config/db"
+import { db } from "./config/db.js"
  dotenv.config()
 const app = express()
 const port = process.env.PORT || 5000

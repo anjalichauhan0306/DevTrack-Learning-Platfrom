766a8663f279ecef02cbbcd261ff018fe928c21a
Anjali Chauhan <ac1447703@gmail.com>
2026-01-29 23:43:28 +0530

    feat: implement getCreatorCourses and editCourse functions for course management

--- server/controller/courseController.js
+++ server/controller/courseController.js
@@ -32,3 +32,50 @@ export const getPublished = async (req,res) => {
     }
 }
 
+export const getCreatorCourses = async (req,res) => {
+    try {
+        const userId = req.userId;
+        const courses = await Courses.find({creator : userId})
+
+        if(!courses){
+            return res.status(400).json({message :"Courses Not Found"})
+        }
+        return res.status(201).json(courses)
+
+    } catch (error) {
+        return res.status(400).json({message : `Failed To Get  creator Courses : ${error}`});
+    }
+}
+
+export const editCourse = async (req,res) => {
+    try {
+        const {courseId} = req.params
+        const {title, subTitle,description , category , level , isPublished , quiz, certificate} = req.body
+
+        let thumbnail 
+
+        if(req.file){
+            thumbnail = await uploadOnCloudinary(req.file.path)
+        }
+
+        let course = await Courses.findById(courseId)
+        if(!course){
+            return res.status(400).json({message : "Courses is Not Found"})
+        }
+
+        const updateData = { title , subTitle , description , category , level , isPublished , price , thumbnail} 
+
+        course = await Courses.findByIdAndUpdate(courseId, updateData , {new :true})
+
+        return res.status(201).json(course)
+
+    } catch (error) {
+        if(!Courses){
+            return res.status(400).json({message : " faild Please try Again"})
+        }
+    }
+}
+
+
+
+
